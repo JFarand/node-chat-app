aecbd8af6cab659775104ef95e09bd6366bf1217
OWL <owlasylum@gmail.com>
2017-06-23 10:06:45 -0400

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -17,19 +17,22 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
 	console.log('New user connected');
 
+	socket.on('createMessage', (message) => {
+		console.log('createMessage', message);
+
+		io.emit('newMessage', {
+			from: message.from,
+			text: message.text,
+			createdAt: new Date().getTime()
+		});
+	});
+
+	
+
 	socket.on('disconnect', () => {
 			console.log('Client disconnected');
 		});
 
-	socket.emit('newMessage', {
-		from: 'owl@example.com',
-		text: 'I love you, Joseph',
-		createdAt: 123123
-	});
-
-	socket.on('createMessage', (message) => {
-		console.log('createMessage', message);
-	});
 });
 
 server.listen(port, () => {
